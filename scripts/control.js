e048a498cb4608420f221a0d4ad16727fc290242
Bernard Rodrigues <bernard@bernard.bernard>
2023-03-07 20:55:35 -0300

    Rating logic and thank you initial layout

--- scripts/control.js
+++ scripts/control.js
@@ -1,3 +1,18 @@
+let rating = 0;
+
+//Função que monitora o click sobre os ratings
 $('#rating div').on('click', function() {
-    $(this).addClass('active')
+    if(!$(this).hasClass('active')){
+        $('#rating div').removeClass('active');
+        $(this).addClass('active');
+        
+        rating = $(this).children('span:first-child').text();
+    }
+})
+
+//Função que monitora o click sobre o botão submit
+$('#submit').on('click', function(){
+    if(rating == 0){
+        alert('Selecione uma nota');
+    }
 })
