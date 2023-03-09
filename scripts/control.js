705c09770da396263e8f1f2ace2cc180852cda6d
Bernard Rodrigues <bernard@bernard.bernard>
2023-03-09 20:46:10 -0300

    Mobile layout finished

--- scripts/control.js
+++ scripts/control.js
@@ -2,17 +2,36 @@ let rating = 0;
 
 //Função que monitora o click sobre os ratings
 $('#rating div').on('click', function() {
+    //Verifica se o botão clicado NÃO possui a classe active
     if(!$(this).hasClass('active')){
+        //Remove a classe active de todos os botões
         $('#rating div').removeClass('active');
+        //Adiciona a classe active no botão clicado
         $(this).addClass('active');
         
+        //Armazena o valor clicado na variável
         rating = $(this).children('span:first-child').text();
     }
 })
 
 //Função que monitora o click sobre o botão submit
 $('#submit').on('click', function(){
+    //Dispara o alerta se nenhuma nota foi escolhida
     if(rating == 0){
-        alert('Selecione uma nota');
+        alert('Please, select your rating!');
+    }else{
+        //Escreve a nota armazenada na variável dentro do span
+        $('#selection span').text(rating);
+
+        //Adiciona as classes de animação
+        $('#rating-container').addClass('animate');
+        $('#thank-you').addClass('animate');
+        
+        /* Aguardar o tempo certo para os elementos sumirem
+        da tela */
+        setTimeout(function(){
+            $('#rating-container').hide();
+            $('#thank-you').css('display', 'flex');
+        }, 250);
     }
 })
